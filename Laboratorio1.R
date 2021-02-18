ddc005a7443ec730dc6b01a81eadbb9c643c7dc3
Andrea <andrealunavasconcelos@hotmail.com>
2021-02-18 00:02:29 -0600

    Laboratorio 1

---
+++ Laboratorio1.R
@@ -0,0 +1,20 @@
+# Gastos totales
+#voy a ingresar los gastos totales de la estudiante universitaria
+300 + 240 + 1527 + 400 + 1500 + 1833
+
+
+celular <- 300
+celular
+transporte <- 240 
+transporte 
+comestibles <- 1527
+comestibles 
+gimnasio <- 400
+gimnasio 
+alquiler <- 1500
+alquiler
+otros <- 1833
+otros
+
+gastos <- c(celular, transporte, comestibles, gimnasio, alquiler, otros)
+barplot(gastos)
