332c8458ce270b96cf6ba194161be041f97c7c44
Andrea1515 <78548889+Andrea1515@users.noreply.github.com>
2021-03-03 08:31:30 -0600

    Add files via upload

---
+++ Tarea1_problema2.R
@@ -0,0 +1,5 @@
+# Andrea Michelle Luna Vasconelos 1950889
+
+germinación <- c(4, 1, 6, 2, 4, 2, 4, 2, 4, 6, 3, 5, 3, 2, 5, 4, 0, 5, 4, 2, 4, 5, 3, 5, 3, 5, 4, 3, 6, 2)
+mean(germinación)
+sd(germinación)
